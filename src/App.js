f0b51548d9eb5dca894c937b6477abb200d429d7
Tayyab <tayyab.21bce7333@vitapstudent.ac.in>
2023-10-19 20:52:59 +0530

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [data, setData] = useState([]);
+  const getData = () => {
+    fetch("https://dummyjson.com/users", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((Data) => {
+        console.log(Data);
+        setData(Data.users);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table className="table table-hover table-bordered">
+        <thead>
+          <tr>
+            <th>S.no</th>
+            <th>Profile picture</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Gender</th>
+            <th>Email</th>
+            <th>Username</th>
+            <th>Domain</th>
+            <th>IP</th>
+            <th>University</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data &&
+            data.length > 0 &&
+            data.map((val, index) => {
+              return (
+                <tr key={index}>
+                  <td>{val.id}</td>
+                  <td>
+                    <img
+                      src={val.image}
+                      alt="Profile"
+                      width="100"
+                      height="100"
+                    />
+                  </td>
+                  <td>{val.firstName}</td>
+                  <td>{val.lastName}</td>
+                  <td>{val.gender}</td>
+                  <td>{val.email}</td>
+                  <td>{val.username}</td>
+                  <td>{val.domain}</td>
+                  <td>{val.ip}</td>
+                  <td>{val.university}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
     </div>
   );
 }
